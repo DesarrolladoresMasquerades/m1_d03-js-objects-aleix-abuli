95cb4a3708ff1d422aed9e39d3349675bf1ce384
Aleix Abulí <aleixabuli088@gmail.com>
2022-01-19 13:05:12 +0100

    Exercise 2 almost finished

---
+++ exercises/02-exe-js_obj_long.js
@@ -0,0 +1,49 @@
+// Iteration 1
+const user = {
+    name: 'Aleix',
+    id: 8
+};
+
+// Iteration 2
+const book1 = {
+    title:  "Black Hole",
+    author: "Charles Burns",
+    isbn: "0375714723",
+    category: "Underground American Comics"
+};
+
+const book2 = {
+    title:  "Confessions of a Mask",
+    author: "Yukio Mishima",
+    isbn: "081120118X",
+    category: "Modern Classic"
+};
+
+// Iteration 3
+user.books = [book1, book2];
+
+// Iteration 4
+const library = [];
+library.push(user);
+
+// Iteration 5
+const book3 = {
+    title:  "The Incal",
+    author: "Moebius",
+    isbn: "1643377809",
+    category: "French BD"
+}
+
+library[0].books.push(book3);
+console.log(library[0]);
+
+//Bonus Iteration
+const user2 = {
+    name: 'Ana',
+    id: 20,
+    books: [book1, book2, book3]
+};
+
+library.push(user2);
+
+for(let i=0;)
